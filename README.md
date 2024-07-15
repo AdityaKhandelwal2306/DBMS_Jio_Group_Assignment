9d4e8a81ed56145f6815b765c3a55311754aa398
YASHBRO <yashjoglekar1220@gmail.com>
2024-07-15 23:29:57 +0530

    Added videos

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # DBMS_Jio_Group_Assignment
+
 Group Assignment: Understanding System Design and Connecting a Database to the Backend of an Application/Product
+
+## MySQL CRUD
+
+<video controls>
+  <source src="./mysql/2024-07-15 23-12-15.mp4" type="video/mp4">
+</video>
+
+## Mongo CRUD
+
+<video controls>
+  <source src="./mongo/2024-07-15 23-16-08.mp4" type="video/mp4">
+</video>
--- mysql/database.py
+++ mysql/database.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 
+
 def create_connection():
     connection = None
     try:
@@ -8,13 +9,14 @@ def create_connection():
             host="localhost",
             user="root",
             password="root",
-            port="30306",
-            database="school"
+            port="3306",
+            database="school",
         )
         print("Connection to MySQL DB successful")
     except Error as e:
         print(f"The error '{e}' occurred")
     return connection
+
 
 def execute_query(connection, query, values=None):
     cursor = connection.cursor()
@@ -27,6 +29,7 @@ def execute_query(connection, query, values=None):
         print("Query executed successfully")
     except Error as e:
         print(f"The error '{e}' occurred")
+
 
 def fetch_query(connection, query, values=None):
     cursor = connection.cursor(dictionary=True)
@@ -42,26 +45,37 @@ def fetch_query(connection, query, values=None):
         print(f"The error '{e}' occurred")
     return None
 
+
 def create_student(connection, name, age, grade):
     query = "INSERT INTO students (name, age, grade) VALUES (%s, %s, %s)"
     values = (name, age, grade)
     execute_query(connection, query, values)
 
+
 def get_students(connection):
     query = "SELECT * FROM students"
     return fetch_query(connection, query)
+
 
 def get_student_by_id(connection, student_id):
     query = "SELECT * FROM students WHERE id = %s"
     values = (student_id,)
     return fetch_query(connection, query, values)
 
+
 def update_student(connection, student_id, name, age, grade):
     query = "UPDATE students SET name = %s, age = %s, grade = %s WHERE id = %s"
     values = (name, age, grade, student_id)
     execute_query(connection, query, values)
 
+
 def delete_student(connection, student_id):
     query = "DELETE FROM students WHERE id = %s"
     values = (student_id,)
     execute_query(connection, query, values)
+
+
+if __name__ == "__main__":
+    print("Testing Connection")
+    conn = create_connection()
+    print(conn)
